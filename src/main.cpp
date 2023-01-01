30266b704e3f502b7a733e3a0e7a012e8c2125da
LiTang <litang0617@outlook.com>
2023-01-01 19:27:57 +0800

    Add:CloseLoop with AS5600

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,103 @@
+/**
+ *
+ * Velocity motion control example
+ * Steps:
+ * 1) Configure the motor and magnetic sensor
+ * 2) Run the code
+ * 3) Set the target velocity (in radians per second) from serial terminal
+ *
+ *
+ * By using the serial terminal set the velocity value you want to motor to obtain
+ *
+ */
 #include <SimpleFOC.h>
-#include <Arduino.h>
 
-// MagneticSensorI2C(uint8_t _chip_address, float _cpr, uint8_t _angle_register_msb)
-//  chip_address  I2C chip address
-//  bit_resolution  resolution of the sensor
-//  angle_register_msb  angle read register msb
-//  bits_used_msb  number of used bits in msb register
-// 
-// make sure to read the chip address and the chip angle register msb value from the datasheet
-// also in most cases you will need external pull-ups on SDA and SCL lines!!!!!
-//
-// For AS5058B
-// MagneticSensorI2C sensor = MagneticSensorI2C(0x40, 14, 0xFE, 8);
 
-// Example of AS5600 configuration 
 MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 TwoWire I2Cone = TwoWire(0);
 
+BLDCMotor motor = BLDCMotor(11);
+BLDCDriver3PWM driver = BLDCDriver3PWM(16, 17, 18);
+
+// velocity set point variable
+float target_velocity = 250;
+// instantiate the commander
+Commander command = Commander(Serial);
+void doTarget(char* cmd) { command.scalar(&target_velocity, cmd); }
 
 void setup() {
-  // monitoring port
-  Serial.begin(115200);
 
-  // configure i2C
-  Wire.setClock(400000);
+  // initialise magnetic sensor hardware
   I2Cone.begin(1,2, 400000UL);   //SDA0,SCL0
   // initialise magnetic sensor hardware
   sensor.init(&I2Cone);
+  // sensor.init();
+  // link the motor to the sensor
+  motor.linkSensor(&sensor);
 
-  Serial.println("Sensor ready");
+  // driver config
+  // power supply voltage [V]
+  driver.voltage_power_supply = 12;
+  driver.init();
+  // link the motor and the driver
+  motor.linkDriver(&driver);
+
+  // set motion control loop to be used
+  motor.controller = MotionControlType::velocity;
+
+  // contoller configuration
+  // default parameters in defaults.h
+
+  // velocity PI controller parameters
+  motor.PID_velocity.P = 0.2f;
+  motor.PID_velocity.I = 20;
+  motor.PID_velocity.D = 0;
+  // default voltage_power_supply
+  motor.voltage_limit = 6;
+  // jerk control using voltage voltage ramp
+  // default value is 300 volts per sec  ~ 0.3V per millisecond
+  motor.PID_velocity.output_ramp = 1000;
+
+  // velocity low pass filtering
+  // default 5ms - try different values to see what is the best.
+  // the lower the less filtered
+  motor.LPF_velocity.Tf = 0.01f;
+
+  // use monitoring with serial
+  Serial.begin(115200);
+  // comment out if not needed
+  motor.useMonitoring(Serial);
+
+  // initialize motor
+  motor.init();
+  // align sensor and start FOC
+  motor.initFOC();
+
+  // add target command T
+  command.add('T', doTarget, "target velocity");
+
+  Serial.println(F("Motor ready."));
+  Serial.println(F("Set the target velocity using serial terminal:"));
   _delay(1000);
 }
 
 void loop() {
-  // iterative function updating the sensor internal variables
-  // it is usually called in motor.loopFOC()
-  // this function reads the sensor hardware and 
-  // has to be called before getAngle nad getVelocity
-  sensor.update();
-  
-  // display the angle and the angular velocity to the terminal
-  Serial.print(sensor.getAngle());
-  Serial.print("\t");
-  Serial.println(sensor.getVelocity());
+  // main FOC algorithm function
+  // the faster you run this function the better
+  // Arduino UNO loop  ~1kHz
+  // Bluepill loop ~10kHz
+  motor.loopFOC();
+
+  // Motion control function
+  // velocity, position or voltage (defined in motor.controller)
+  // this function can be run at much lower frequency than loopFOC() function
+  // You can also use motor.move() and set the motor.target in the code
+  motor.move(target_velocity);
+
+  // function intended to be used with serial plotter to monitor motor variables
+  // significantly slowing the execution down!!!!
+  // motor.monitor();
+
+  // user communication
+  command.run();
 }
