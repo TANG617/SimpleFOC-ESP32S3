010e00faccfc1e7a006d5017274fccd429d53bcf
LiTang <litang0617@outlook.com>
2023-01-12 15:55:07 +0800

    Serial Tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,107 +1,12 @@
-/**
- *
- * Velocity motion control example
- * Steps:
- * 1) Configure the motor and magnetic sensor
- * 2) Run the code
- * 3) Set the target velocity (in radians per second) from serial terminal
- *
- *
- * By using the serial terminal set the velocity value you want to motor to obtain
- *
- */
-#include <SimpleFOC.h>
-
-
-MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
-TwoWire I2Cone = TwoWire(0);
-
-BLDCMotor motor = BLDCMotor(11);
-BLDCDriver3PWM driver = BLDCDriver3PWM(2, 3, 4,1);
-
-// velocity set point variable
-float target_velocity = 10;
-// instantiate the commander
-Commander command = Commander(Serial);
-void doTarget(char* cmd) { command.scalar(&target_velocity, cmd); }
-
-void setup() {
-    // use monitoring with serial
-  Serial.begin(115200);
-  Serial.println("OK");
-
-  // initialise magnetic sensor hardware
-  I2Cone.begin(10,9, 400000UL);   //SDA0,SCL0
-  // initialise magnetic sensor hardware
-  sensor.init(&I2Cone);
-  // sensor.init();
-  // link the motor to the sensor
-  motor.linkSensor(&sensor);
-
-  // driver config
-  // power supply voltage [V]
-  driver.voltage_power_supply = 12;
-  driver.init();
-  // link the motor and the driver
-  motor.linkDriver(&driver);
-
-  // set motion control loop to be used
-  motor.controller = MotionControlType::velocity;
-
-  // contoller configuration
-  // default parameters in defaults.h
-
-  // velocity PI controller parameters
-  motor.PID_velocity.P = 0.2f;
-  motor.PID_velocity.I = 20;
-  motor.PID_velocity.D = 0;
-  // default voltage_power_supply
-  motor.voltage_limit = 12;
-  // jerk control using voltage voltage ramp
-  // default value is 300 volts per sec  ~ 0.3V per millisecond
-  motor.PID_velocity.output_ramp = 1000;
-
-  // velocity low pass filtering
-  // default 5ms - try different values to see what is the best.
-  // the lower the less filtered
-  motor.LPF_velocity.Tf = 0.01f;
-
-
-  // comment out if not needed
-  motor.useMonitoring(Serial);
-
-  // initialize motor
-  motor.init();
-  // align sensor and start FOC
-  motor.initFOC();
-
-  // add target command T
-  command.add('T', doTarget, "target velocity");
-  motor.useMonitoring(Serial);
-  Serial.println(F("Motor ready."));
-  Serial.println(F("Set the target velocity using serial terminal:"));
-  _delay(1000);
+//TEST FOR SERIAL PRINT
+#include<Arduino.h>
+void setup()
+{
+  Serial0.begin(115200);
 }
 
-void loop() {
-  // main FOC algorithm function
-  // the faster you run this function the better
-  // Arduino UNO loop  ~1kHz
-  // Bluepill loop ~10kHz
-  motor.loopFOC();
-
-  // motor.monitor();
-
-  // Motion control function
-  // velocity, position or voltage (defined in motor.controller)
-  // this function can be run at much lower frequency than loopFOC() function
-  // You can also use motor.move() and set the motor.target in the code
-  motor.move(target_velocity);
-  Serial.println(sensor.getSensorAngle());
-  // function intended to be used with serial plotter to monitor motor variables
-  // significantly slowing the execution down!!!!
-  // motor.monitor();
-
-  // user communication
-  command.run();
+void loop()
+{
+  Serial0.println("TEST,OK");
+  delay(1000);
 }
