88620babc970891ea17ae5fab99cb70949a81d33
LiTang <litang0617@outlook.com>
2023-01-30 21:08:57 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -111,6 +111,7 @@ void setup() {
 }
 
 
+<<<<<<< Updated upstream
 void loop() {
   // iterative setting FOC phase voltage
   motor.loopFOC();
@@ -234,3 +235,54 @@ void loop() {
 //   // user communication
 //   command.run();
 // }
+=======
+void loop()
+{
+    delay(5000);
+    // ++value;
+
+    // Serial.print("connecting to ");
+    // Serial.println(host);
+
+    // // Use WiFiClient class to create TCP connections
+    // WiFiClient client;
+    // const int httpPort = 80;
+    // if (!client.connect(host, httpPort)) {
+    //     Serial.println("connection failed");
+    //     return;
+    // }
+
+    // // We now create a URI for the request
+    // String url = "/input/";
+    // url += streamId;
+    // url += "?private_key=";
+    // url += privateKey;
+    // url += "&value=";
+    // url += value;
+
+    // Serial.print("Requesting URL: ");
+    // Serial.println(url);
+
+    // // This will send the request to the server
+    // client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+    //              "Host: " + host + "\r\n" +
+    //              "Connection: close\r\n\r\n");
+    // unsigned long timeout = millis();
+    // while (client.available() == 0) {
+    //     if (millis() - timeout > 5000) {
+    //         Serial.println(">>> Client Timeout !");
+    //         client.stop();
+    //         return;
+    //     }
+    // }
+
+    // // Read all the lines of the reply from server and print them to Serial
+    // while(client.available()) {
+    //     String line = client.readStringUntil('\r');
+    //     Serial.print(line);
+    // }
+
+    // Serial.println();
+    // Serial.println("closing connection");
+}
+>>>>>>> Stashed changes
